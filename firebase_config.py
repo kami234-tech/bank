546502e8dce4bcb265c34bbcbce97f24debdbcbf
kami234-tech <marekami234@gmail.com>
2025-06-07 11:54:41 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,14 @@
 import os
 import json
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials
 
-cred_json = os.environ.get("FIREBASE_ADMIN_CREDENTIALS")
-cred = credentials.Certificate(json.loads(cred_json))
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
+# Load the Firebase Admin credentials from the environment variable
+firebase_credentials_json = os.environ.get('FIREBASE_ADMIN_CREDENTIALS')
+if firebase_credentials_json:
+    firebase_credentials = json.loads(firebase_credentials_json)
+    cred = credentials.Certificate(firebase_credentials)
+    firebase_admin.initialize_app(cred)
+else:
+    # Handle the case where the environment variable is not set
+    raise ValueError("FIREBASE_ADMIN_CREDENTIALS environment variable is not set.")
